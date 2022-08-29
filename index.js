3e6a4d15f1a08c851fa306ac0ab00a79eb54495e
TranTam <tamvaa1306@gmail.com>
2022-08-29 17:26:31 +0700

    install morgan

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
 const express = require("express");
+const morgan = require("morgan");
+
 const app = express();
 const port = 3000;
 
+app.use(morgan("combined"));
+
 app.get("/", (req, res) => {
-  res.send("Hello World!!");
+  res.send("<h1>Hello World!</h1>");
 });
 
 app.listen(port, () => {
