75e189b9375c4752727f4a3ddff01471817fbf4f
TranTam <tamvaa1306@gmail.com>
2022-08-29 17:41:38 +0700

    install handlebars

--- src/index.js
+++ src/index.js
@@ -1,10 +1,16 @@
 const express = require("express");
 const morgan = require("morgan");
+const handlebars = require("handlebars");
 
 const app = express();
 const port = 3000;
 
+// HTTP logger
 app.use(morgan("combined"));
+
+// Template engines
+app.engine("handlebars", handlebars());
+app.set("view engine", "handlebars");
 
 app.get("/", (req, res) => {
   res.send("<h1>Hello World!</h1>");
